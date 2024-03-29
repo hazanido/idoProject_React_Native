ac7c2289701190c0d8397c9c0833d987442ca278
hazanido <hazanido@gmail.com>
2024-03-29 16:24:05 +0300

    up package

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 const express = require("express")
 const app = express();
-
-const port = 3000;
+const dotenv = require("dotenv").config();
 
 app.get('/', (req, res) => {
     res.send('SERVER STARTED!')
 })
 
-app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
+app.listen(process.env.PORT, () => {
+    console.log(`http://localhost:${process.env.PORT}`)
 })
