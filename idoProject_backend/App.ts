2ae20c5163494c60672934df248e78fc1ba0264b
hazanido <hazanido@gmail.com>
2024-04-24 18:22:25 +0300

    change ts

---
+++ idoProject_backend/App.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import user from './Model/userModel';
+import user_route from './routes/user_route.js';
+import post_route from './routes/post_route.js';
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const appInit = () =>{
+   const promise = new Promise((resolve) => {
+    
+        try {
+            console.log('Trying to connect to DB');
+            mongoose.connect(process.env.DB_CONNECT);
+            console.log('Connected to MongoDB.');
+        } catch (error) {
+            console.log('Error connecting to DB: ' + error);
+        }
+    
+    app.use('/user',user_route);
+    app.use('/post',post_route);
+    
+    app.get('/', (req, res) => {
+        res.send('SERVER STARTED!');
+    });
+
+    resolve(app);
+   });
+   return promise;
+};
+
+
+
+
+module.exports = appInit;
---
+++ idoProject_backend/dist/App.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const dotenv = require('dotenv').config();
+const mongoose_1 = __importDefault(require("mongoose"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const user_route_js_1 = __importDefault(require("./routes/user_route.js"));
+const post_route_js_1 = __importDefault(require("./routes/post_route.js"));
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(body_parser_1.default.json());
+const appInit = () => {
+    const promise = new Promise((resolve) => {
+        try {
+            console.log('Trying to connect to DB');
+            mongoose_1.default.connect(process.env.DB_CONNECT);
+            console.log('Connected to MongoDB.');
+        }
+        catch (error) {
+            console.log('Error connecting to DB: ' + error);
+        }
+        app.use('/user', user_route_js_1.default);
+        app.use('/post', post_route_js_1.default);
+        app.get('/', (req, res) => {
+            res.send('SERVER STARTED!');
+        });
+        resolve(app);
+    });
+    return promise;
+};
+module.exports = appInit;
+//# sourceMappingURL=App.js.map
