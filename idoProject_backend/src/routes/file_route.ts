828a46ca39ab6cee12ac3ecad677d0c31a690d80
hazanido <hazanido@gmail.com>
2024-06-18 23:14:01 +0300

    add test for file

--- idoProject_backend/src/routes/file_route.ts
+++ idoProject_backend/src/routes/file_route.ts
@@ -1,13 +1,18 @@
+// src/routes/file_route.ts
+
 import multer from 'multer';
-import express, { Request } from 'express';
+import express, { Request, Response } from 'express';
 import path from 'path';
+
 const router = express.Router();
 
 const base = "http://localhost:3000/";
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../../imageUser'));
+        const destPath = path.join(__dirname, '../../imageUser');
+        console.log(`Destination path: ${destPath}`);
+        cb(null, destPath);
     },
     filename: (req, file, cb) => {
         const filename = Date.now() + '.jpg';
@@ -18,7 +23,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-router.post('/upload', upload.single('file'), (req: Request & { file: Express.Multer.File }, res) => {
+router.post('/upload', upload.single('file'), (req: Request & { file: Express.Multer.File }, res: Response) => {
     try {
         if (!req.file) {
             console.log('No file uploaded.');
@@ -26,7 +31,8 @@ router.post('/upload', upload.single('file'), (req: Request & { file: Express.Mu
         }
 
         const filePath = path.relative(__dirname, req.file.path);
-        const fileUrl = `${base}idoProject_backend/imageUser/${filePath}`;
+        const fileUrl = `${base}idoProject_backend/imageUser/${filePath.replace(/\\/g, '/')}`;
+        console.log(`File uploaded: ${fileUrl}`);
         res.status(200).send({ url: fileUrl });
     } catch (error) {
         console.error('Error handling file upload:', error);
